1d70ce3ec084e2d035fa629f70e5f0811a2122b9
Matheus Cristian <matheuscristianrg@gmail.com>
2026-08-02 02:03:24 -0300

    update for ssh connections

--- images/dev-arch/entrypoint.sh
+++ images/dev-arch/entrypoint.sh
@@ -7,5 +7,8 @@ sudo chown -R dev:dev /home/dev
 mkdir -p /workspace
 sudo chown -R dev:dev /workspace
 
+echo "Initializing sshd daemon..."
+sudo /usr/bin/sshd
+
 echo "Staring shell as dev user..."
 exec "$SHELL"
