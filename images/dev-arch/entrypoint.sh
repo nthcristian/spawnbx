2f414caf73143c50368900b6c54df4dfa803c72f
Matheus Cristian <matheuscristianrg@gmail.com>
2026-08-02 02:08:03 -0300

    update entrypoint.sh

--- images/dev-arch/entrypoint.sh
+++ images/dev-arch/entrypoint.sh
@@ -7,7 +7,7 @@ sudo chown -R dev:dev /home/dev
 mkdir -p /workspace
 sudo chown -R dev:dev /workspace
 
-echo "Initializing sshd daemon..."
+echo "Starting sshd daemon..."
 sudo /usr/bin/sshd
 
 echo "Staring shell as dev user..."
